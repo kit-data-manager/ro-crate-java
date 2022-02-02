3d283bfc4b899fb9a13adb3df4d2c8389c525264
uksgf <nikola.tzotchev@student.kit.edu>
2022-02-02 21:34:55 +0100

    Add testTest to test the pipeline

---
+++ src/test/java/edu/kit/rocrate/TestExample.java
@@ -0,0 +1,17 @@
+package edu.kit.rocrate;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author Nikola Tzotchev on 2.2.2022 г.
+ * @version 1
+ */
+public class TestExample {
+
+  @Test
+  void testTest () {
+    assertEquals(1,1);
+  }
+}
