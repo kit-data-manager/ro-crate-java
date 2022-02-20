5f344a92b3c0be8857d319e59bfba884256e5c0e
nikolatzotchev <ntzotchev@gmail.com>
2022-02-21 01:01:22 +0200

    Test

---
+++ src/test/java/edu/kit/rocrate/crate/TestCase.java
@@ -0,0 +1,13 @@
+package edu.kit.rocrate.crate;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestCase {
+
+  @Test
+  void testFalse() {
+    assertEquals(2, 1);
+  }
+}
