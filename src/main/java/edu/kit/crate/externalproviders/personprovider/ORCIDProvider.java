d8ab1a1b228ba07fa3d3b3448806557800081220
nikolatzotchev <ntzotchev@gmail.com>
2022-03-24 10:56:05 +0200

    Orcid jsonld was returning an invalid json for entity (it contains nested elements) decided to only include what is possible

--- src/main/java/edu/kit/crate/externalproviders/personprovider/ORCIDProvider.java
+++ src/main/java/edu/kit/crate/externalproviders/personprovider/ORCIDProvider.java
@@ -1,10 +1,23 @@
 package edu.kit.crate.externalproviders.personprovider;
 
+import com.apicatalog.jsonld.JsonLd;
+import com.apicatalog.jsonld.JsonLdError;
+import com.apicatalog.jsonld.JsonLdOptions;
+import com.apicatalog.jsonld.document.Document;
+import com.apicatalog.jsonld.document.JsonDocument;
+import com.apicatalog.jsonld.json.JsonUtils;
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import edu.kit.crate.entities.contextual.PersonEntity;
+import edu.kit.crate.entities.validation.EntityValidation;
+import edu.kit.crate.entities.validation.JsonSchemaValidation;
 import edu.kit.crate.objectmapper.MyObjectMapper;
+
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
 import org.apache.http.HttpHeaders;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
@@ -22,16 +35,25 @@ public class ORCIDProvider {
     if (!url.matches("https://orcid.org.*")) {
       throw new IllegalArgumentException("Should provide orcid link");
     }
-    // TODO: check if ulr is orcid url
     HttpGet request = new HttpGet(url);
     request.addHeader(HttpHeaders.ACCEPT, "application/ld+json");
     try {
       CloseableHttpResponse response = httpClient.execute(request);
-      ObjectNode jsonNode = MyObjectMapper.getMapper().readValue(response.getEntity().getContent(),
+      ObjectMapper objectMapper = MyObjectMapper.getMapper();
+      ObjectNode jsonNode = objectMapper.readValue(response.getEntity().getContent(),
           ObjectNode.class);
       jsonNode.remove("@reverse");
       jsonNode.remove("@context");
-      return new PersonEntity.PersonEntityBuilder().setAll(jsonNode).build();
+      ObjectNode node = objectMapper.createObjectNode();
+      EntityValidation entityValidation = new EntityValidation(new JsonSchemaValidation());
+      var itr = jsonNode.fields();
+      while (itr.hasNext()) {
+        var element = itr.next();
+        if (entityValidation.fieldValidation(element.getValue())) {
+          node.set(element.getKey(), element.getValue());
+        }
+      }
+      return new PersonEntity.PersonEntityBuilder().setAll(node).build();
     } catch (IOException e) {
       e.printStackTrace();
       return null;
