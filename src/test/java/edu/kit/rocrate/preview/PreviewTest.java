943e74e5f4e0d63597965633b5908edc1e2c37b7
nikolatzotchev <ntzotchev@gmail.com>
2022-04-06 03:43:45 +0300

    +

--- src/test/java/edu/kit/rocrate/preview/PreviewTest.java
+++ src/test/java/edu/kit/rocrate/preview/PreviewTest.java
@@ -1,15 +1,15 @@
 package edu.kit.rocrate.preview;
 
-import edu.kit.crate.ROCrate;
 import edu.kit.crate.preview.AutomaticPreview;
 import edu.kit.crate.preview.CustomPreview;
-import edu.kit.crate.writer.FolderWriter;
-import edu.kit.crate.writer.ROCrateWriter;
+import net.lingala.zip4j.ZipFile;
+import net.lingala.zip4j.model.ZipParameters;
 import org.apache.commons.io.FileUtils;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -20,57 +20,99 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 public class PreviewTest {
 
   @Test
-  void testAutomaticPreview(@TempDir Path temp) throws IOException {
-    Path location = temp.resolve("ro_crate1");
-    ROCrate crate = new ROCrate.ROCrateBuilder("name", "description")
-        .setPreview(new AutomaticPreview())
-        .build();
-    ROCrateWriter writer = new ROCrateWriter(new FolderWriter());
-    writer.save(crate, location.toFile().getAbsolutePath());
-    assertTrue(Files.isRegularFile(location.resolve("ro-crate-preview.html")));
+  void customPreviewSaveToFolderTest(@TempDir Path dir) throws IOException {
+    var file1 = dir.resolve("file.html");
+    FileUtils.writeStringToFile(file1.toFile(), "random html it is not important that it is valid for know", Charset.defaultCharset());
+
+    var file2 = dir.resolve("directory");
+    var fileInDir = file2.resolve("fileInDir.html");
+    FileUtils.writeStringToFile(fileInDir.toFile(), "dajkdlfjdsklafj alksfjdalk fjl", Charset.defaultCharset());
+    CustomPreview customPreview = new CustomPreview(file1.toFile(), file2.toFile());
+
+    customPreview.saveALLToFolder(dir.resolve("result").toFile());
+
+    var e = dir.resolve("result");
+
+    var roPreview = e.resolve("ro-crate-preview.html");
+    var roDir = e.resolve("ro-crate-preview_files");
+    var roDirFile = roDir.resolve("fileInDir.html");
+    assertTrue(Files.isRegularFile(roPreview));
+    assertTrue(Files.isDirectory(roDir));
+    assertTrue(Files.isRegularFile(roDirFile));
+
+    assertTrue(FileUtils.contentEqualsIgnoreEOL(roPreview.toFile(), file1.toFile(), String.valueOf(Charset.defaultCharset())));
+    assertFalse(FileUtils.contentEqualsIgnoreEOL(roPreview.toFile(), fileInDir.toFile(), String.valueOf(Charset.defaultCharset())));
+
+    assertTrue(FileUtils.contentEqualsIgnoreEOL(roDirFile.toFile(), fileInDir.toFile(), String.valueOf(Charset.defaultCharset())));
   }
 
   @Test
-  void testAutomaticPreviewAddingLater(@TempDir Path temp) {
-    Path location = temp.resolve("ro_crate2");
-    ROCrate crate = new ROCrate.ROCrateBuilder("name", "description").build();
-    ROCrateWriter writer = new ROCrateWriter(new FolderWriter());
-    writer.save(crate, location.toFile().toString());
-    assertFalse(location.resolve("ro-crate-preview.html").toFile().exists());
-    crate.setRoCratePreview(new AutomaticPreview());
-    writer.save(crate, location.toFile().toString());
-    assertTrue(location.resolve("ro-crate-preview.html").toFile().exists());
+  void customPreviewSaveToZip(@TempDir Path dir) throws IOException {
+    var file1 = dir.resolve("file.html");
+    FileUtils.writeStringToFile(file1.toFile(), "random html it is not important that it is valid for know", Charset.defaultCharset());
+
+    var file2 = dir.resolve("directory");
+    var fileInDir = file2.resolve("fileInDir.html");
+    FileUtils.writeStringToFile(fileInDir.toFile(), "dajkdlfjdsklafj alksfjdalk fjl", Charset.defaultCharset());
+    CustomPreview customPreview = new CustomPreview(file1.toFile(), file2.toFile());
+
+    customPreview.saveALLToZip(new ZipFile(dir.resolve("destination.zip").toFile()));
+    new ZipFile(dir.resolve("destination.zip").toFile()).extractAll(dir.resolve("extracted").toAbsolutePath().toString());
+
+    var e = dir.resolve("extracted");
+    var roPreview = e.resolve("ro-crate-preview.html");
+    var roDir = e.resolve("ro-crate-preview_files");
+    var roDirFile = roDir.resolve("fileInDir.html");
+    assertTrue(Files.isRegularFile(roPreview));
+    assertTrue(Files.isDirectory(roDir));
+    assertTrue(Files.isRegularFile(roDirFile));
+
+    assertTrue(FileUtils.contentEqualsIgnoreEOL(roPreview.toFile(), file1.toFile(), String.valueOf(Charset.defaultCharset())));
+    assertFalse(FileUtils.contentEqualsIgnoreEOL(roPreview.toFile(), fileInDir.toFile(), String.valueOf(Charset.defaultCharset())));
+
+    assertTrue(FileUtils.contentEqualsIgnoreEOL(roDirFile.toFile(), fileInDir.toFile(), String.valueOf(Charset.defaultCharset())));
+
   }
 
   @Test
-  void testCustomPreviewOnlyHtmlFile(@TempDir Path temp) throws IOException {
-    Path location = temp.resolve("ro_crate3");
-    Path previewFile = temp.resolve("random.html");
-    FileUtils.writeStringToFile(previewFile.toFile(), "random html it is not important that it is valid for know", Charset.defaultCharset());
-    ROCrate crate = new ROCrate.ROCrateBuilder("name", "description")
-        .setPreview(new CustomPreview(previewFile.toFile()))
-        .build();
-    ROCrateWriter writer = new ROCrateWriter(new FolderWriter());
-    writer.save(crate, location.toFile().toString());
-    assertTrue(location.resolve("ro-crate-preview.html").toFile().exists());
+  void testAutomaticPreviewAddToFolder(@TempDir Path dir) throws IOException {
+    AutomaticPreview automaticPreview = new AutomaticPreview();
+
+    InputStream crateJson = PreviewTest.class.getResourceAsStream("/crates/simple_crate/ro-crate-metadata.json");
+    Path crate = dir.resolve("crate");
+    // this crate will not have a json file
+    Path fakeCrate = dir.resolve("fakeCrate");
+    FileUtils.forceMkdir(crate.toFile());
+    FileUtils.copyInputStreamToFile(crateJson, crate.resolve("ro-crate-metadata.json").toFile());
+
+    automaticPreview.saveALLToFolder(crate.toFile());
+
+    // the program should not crash
+    automaticPreview.saveALLToFolder(fakeCrate.toFile());
+
+    // there should be a html file generated
+    assertTrue(Files.isRegularFile(crate.resolve("ro-crate-preview.html")));
   }
 
   @Test
-  void testCustomPreviewOnlyHtmlFileWithOtherFiles(@TempDir Path temp) throws IOException {
-    Path location = temp.resolve("ro_crate4");
-    Path previewFile = temp.resolve("random.html");
-    FileUtils.writeStringToFile(previewFile.toFile(), "random html it is not important that it is valid for know", Charset.defaultCharset());
-    Path dirHtml = temp.resolve("html_dir");
-    Path css_file = dirHtml.resolve("test.css");
-    FileUtils.writeStringToFile(css_file.toFile(), "random css it is not important that it is valid for know", Charset.defaultCharset());
-    ROCrate crate = new ROCrate.ROCrateBuilder("name", "description")
-        .setPreview(new CustomPreview(previewFile.toFile(), dirHtml.toFile()))
-        .build();
-    ROCrateWriter writer = new ROCrateWriter(new FolderWriter());
-    writer.save(crate, location.toFile().toString());
-    assertTrue(location.resolve("ro-crate-preview.html").toFile().exists());
-    assertTrue(location.resolve("ro-crate-preview_files").toFile().exists());
-    assertTrue(location.resolve("ro-crate-preview_files").resolve("test.css").toFile().exists());
-  }
+  void testAutomaticPreviewZip(@TempDir Path dir) throws IOException {
+    AutomaticPreview automaticPreview = new AutomaticPreview();
+    InputStream crateJson = PreviewTest.class.getResourceAsStream("/crates/simple_crate/ro-crate-metadata.json");
+    Path crate = dir.resolve("crate");
+    ZipParameters zipParameters = new ZipParameters();
+    zipParameters.setFileNameInZip("ro-crate-metadata.json");
 
+    ZipFile zipFile = new ZipFile(dir.resolve("test.zip").toFile());
+    zipFile.addStream(crateJson, zipParameters);
+    crateJson.close();
+
+    automaticPreview.saveALLToZip(zipFile);
+
+    // this should trow an exception but not stop the execution
+    ZipFile randomZipFile = new ZipFile(dir.resolve("dddd.zip").toFile());
+    automaticPreview.saveALLToZip(randomZipFile);
+
+    zipFile.extractAll(crate.toString());
+    assertTrue(Files.isRegularFile(crate.resolve("ro-crate-preview.html")));
+  }
 }
