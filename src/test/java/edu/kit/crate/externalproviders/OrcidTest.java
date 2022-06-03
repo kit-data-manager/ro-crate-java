6c9f9841c130935ba9dfd9525385e5d9aaf1ea4e
Jonas Scholz <jonas.scholz@bbscholz.de>
2022-06-04 00:30:42 +0200

    Rename and add test cases

--- src/test/java/edu/kit/crate/externalproviders/OrcidTest.java
+++ src/test/java/edu/kit/crate/externalproviders/OrcidTest.java
@@ -6,15 +6,32 @@ import edu.kit.crate.HelpFunctions;
 import java.io.IOException;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 /**
  * @author Nikola Tzotchev on 10.2.2022 г.
  * @version 1
  */
-public class ORCIDTest {
+public class OrcidTest {
 
   @Test
   void testAddingPersonEntity() throws IOException {
     PersonEntity person = OrcidProvider.getPerson("https://orcid.org/0000-0001-9842-9718");
     HelpFunctions.compareEntityWithFile(person, "/json/entities/contextual/orcidperson.json");
   }
+
+  @Test
+  void testInvalidOrcidUrl() {
+    Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+      PersonEntity person = OrcidProvider.getPerson("https://notorcid.org/1234");
+    });
+  }
+
+  @Test
+  void testInvalidOrcidId() {
+    PersonEntity person = OrcidProvider.getPerson("https://orcid.org/42");
+    assertNull(person);
+  }
+
 }
--- src/test/java/edu/kit/crate/externalproviders/RorProviderTest.java
+++ src/test/java/edu/kit/crate/externalproviders/RorProviderTest.java
@@ -7,6 +7,9 @@ import java.io.IOException;
 import edu.kit.crate.HelpFunctions;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 /**
  * @author Nikola Tzotchev on 11.2.2022 г.
  * @version 1
@@ -17,5 +20,18 @@ public class RORProviderTest {
   void testExternalRORProvider() throws IOException {
     OrganizationEntity organizationEntity = RorProvider.getOrganization("https://ror.org/04t3en479");
     HelpFunctions.compareEntityWithFile(organizationEntity, "/json/entities/contextual/rorkit.json");
+  }
+
+  @Test
+  void testInvalidRORUrl() {
+    Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+      OrganizationEntity organizationEntity = RorProvider.getOrganization("https://notror.org/04t3en479");
+    });
+  }
+
+  @Test
+  void testInvalidRORId() {
+    OrganizationEntity organizationEntity = RorProvider.getOrganization("https://ror.org/42");
+    assertNull(organizationEntity);
   }
 }
