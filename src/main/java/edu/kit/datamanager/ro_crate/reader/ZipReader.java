4a2aa9baa97d1c8436eeb0548483429b8ca9748a
Andreas Pfeil <andreas.pfeil@kit.edu>
2024-09-13 14:32:50 +0200

    feat: add getters for information useful for manual removal of temporary files.

--- src/main/java/edu/kit/datamanager/ro_crate/reader/ZipReader.java
+++ src/main/java/edu/kit/datamanager/ro_crate/reader/ZipReader.java
@@ -21,14 +21,23 @@ import org.apache.commons.io.FileUtils;
  * By default, it will be `./.tmp/ro-crate-java/zipReader/$UUID/`.
  * <p>
  * NOTE: The resulting crate may refer to these temporary files. Therefore,
- * these files are only being deleted before the JVM exits.
+ * these files are only being deleted before the JVM exits. If you need to free
+ * space because your application is long-running or creates a lot of
+ * crates, you may use the getters to retrieve information which will help
+ * you to clean up manually. Keep in mind that crates may refer to this
+ * folder after extraction. Use RoCrateWriter to export it so some
+ * persistent location and possibly read it from there, if required. Or use
+ * the ZipWriter to write it back to its source.
  */
 public class ZipReader implements ReaderStrategy {
 
   protected final String ID = UUID.randomUUID().toString();
-  protected Path tempFolder = Path.of(String.format("./.tmp/ro-crate-java/zipReader/%s/", ID));
-  protected boolean read = false;
+  protected Path temporaryFolder = Path.of(String.format("./.tmp/ro-crate-java/zipReader/%s/", ID));
+  protected boolean isExtracted = false;
 
+  /**
+   * Crates a ZipReader with the default configuration as described in the class documentation.
+   */
   public ZipReader() {}
 
   /**
@@ -44,15 +53,36 @@ public class ZipReader implements ReaderStrategy {
    */
   public ZipReader(Path folderPath, boolean shallAddUuidSubfolder) {
     if (shallAddUuidSubfolder) {
-      this.tempFolder = folderPath.resolve(ID);
+      this.temporaryFolder = folderPath.resolve(ID);
     } else {
-      this.tempFolder = folderPath;
+      this.temporaryFolder = folderPath;
     }
+  }
+
+  /**
+   * @return the identifier which may be used as the name for a subfolder in the temporary directory.
+   */
+  public String getID() {
+    return ID;
+  }
+
+  /**
+   * @return the folder (considered temporary) where the zipped crate will be or has been extracted to.
+   */
+  public Path getTemporaryFolder() {
+    return temporaryFolder;
+  }
+
+  /**
+   * @return whether the crate has already been extracted into the temporary folder.
+   */
+  public boolean isExtracted() {
+    return isExtracted;
   }
 
   private void readCrate(String location) {
     try {
-      File folder = tempFolder.toFile();
+      File folder = temporaryFolder.toFile();
       // ensure the directory is clean
       if (folder.isDirectory()) {
         FileUtils.cleanDirectory(folder);
@@ -61,8 +91,8 @@ public class ZipReader implements ReaderStrategy {
       }
       // extract
       try (ZipFile zf = new ZipFile(location)) {
-        zf.extractAll(tempFolder.toAbsolutePath().toString());
-        this.read = true;
+        zf.extractAll(temporaryFolder.toAbsolutePath().toString());
+        this.isExtracted = true;
       }
       // register deletion on exit
       FileUtils.forceDeleteOnExit(folder);
@@ -73,12 +103,12 @@ public class ZipReader implements ReaderStrategy {
 
   @Override
   public ObjectNode readMetadataJson(String location) {
-    if (!read) {
+    if (!isExtracted) {
       this.readCrate(location);
     }
 
     ObjectMapper objectMapper = MyObjectMapper.getMapper();
-    File jsonMetadata = tempFolder.resolve("ro-crate-metadata.json").toFile();
+    File jsonMetadata = temporaryFolder.resolve("ro-crate-metadata.json").toFile();
     
     try {
       return objectMapper.readTree(jsonMetadata).deepCopy();
@@ -90,9 +120,9 @@ public class ZipReader implements ReaderStrategy {
 
   @Override
   public File readContent(String location) {
-    if (!read) {
+    if (!isExtracted) {
       this.readCrate(location);
     }
-    return tempFolder.toFile();
+    return temporaryFolder.toFile();
   }
 }
