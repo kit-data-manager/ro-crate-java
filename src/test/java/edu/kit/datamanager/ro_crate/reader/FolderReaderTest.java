bef5f67fb5351e591d6f7f6444c21f3bf2f0ca2c
Andreas Pfeil <andreas.pfeil.rs@gmail.com>
2025-04-25 16:01:17 +0200

    refactor(test): Unify reader tests and use new API

--- src/test/java/edu/kit/datamanager/ro_crate/reader/FolderReaderTest.java
+++ src/test/java/edu/kit/datamanager/ro_crate/reader/FolderReaderTest.java
@@ -4,19 +4,12 @@ import edu.kit.datamanager.ro_crate.Crate;
 import edu.kit.datamanager.ro_crate.HelpFunctions;
 import edu.kit.datamanager.ro_crate.RoCrate;
 import edu.kit.datamanager.ro_crate.entities.contextual.PersonEntity;
-import edu.kit.datamanager.ro_crate.entities.data.FileEntity;
-
-import edu.kit.datamanager.ro_crate.writer.FolderWriter;
-import edu.kit.datamanager.ro_crate.writer.RoCrateWriter;
-
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.PrintStream;
-import java.nio.charset.Charset;
-import java.nio.file.Path;
-import org.apache.commons.io.FileUtils;
+import edu.kit.datamanager.ro_crate.writer.Writers;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
+
+import java.io.IOException;
+import java.nio.file.Path;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -24,49 +17,54 @@ import static org.junit.jupiter.api.Assertions.*;
  * @author Nikola Tzotchev on 9.2.2022 г.
  * @version 1
  */
-class FolderReaderTest {
+class FolderReaderTest extends CrateReaderTest<String, FolderStrategy> {
 
-  private Path writeMetadataToFile(Path temp, RoCrate c1) throws IOException {
-    // Write metadata to file
-    Path f = temp.resolve("ro-crate-metadata.json");
-    FileUtils.touch(f.toFile());
-    FileUtils.writeStringToFile(f.toFile(), c1.getJsonMetadata(), Charset.defaultCharset());
-    return f;
+  @Override
+  protected void saveCrate(Crate crate, Path target) {
+    Writers.newFolderWriter().save(crate, target.toAbsolutePath().toString());
+    assertTrue(target.toFile().isDirectory());
   }
 
-  @Test
-  void testReadingBasicCrate(@TempDir Path temp) throws IOException {
-    RoCrate roCrate = new RoCrate.RoCrateBuilder("minimal", "minimal RO_crate", "2024", "https://creativecommons.org/licenses/by-nc-sa/3.0/au/")
-        .build();
-    Path f = writeMetadataToFile(temp, roCrate);
-    // Read from written file
-    CrateReader<String> reader = Readers.newFolderReader();
-    RoCrate res = reader.readCrate(temp.toFile().toString());
-    // Write metadata again
-    Path r = temp.resolve("output.txt");
-    FileUtils.touch(r.toFile());
-    FileUtils.writeStringToFile(r.toFile(), res.getJsonMetadata(), Charset.defaultCharset());
-    // See if it is the same
-    assertTrue(FileUtils.contentEquals(f.toFile(), r.toFile()));
+  @Override
+  protected Crate readCrate(Path source) throws IOException {
+    return Readers.newFolderReader().readCrate(source.toAbsolutePath().toString());
   }
 
+  @Override
+  protected FolderStrategy newReaderStrategyWithTmp(Path tmpDirectory, boolean useUuidSubfolder) {
+    // This strategy does not support a non-default temporary directory
+    // and will always use the default one.
+    // It also has no state we could make assertions on.
+    return new FolderStrategy();
+  }
+
+  @Override
+  protected Crate readCrate(FolderStrategy strategy, Path source) throws IOException {
+      return new CrateReader<>(strategy)
+          .readCrate(source.toAbsolutePath().toString());
+  }
+
+  /**
+   * The folder reader is state-less, so we should be able to read multiple crates
+   * with the same instance.
+   */
   @Test
   void testMultipleReads(@TempDir Path temp1, @TempDir Path temp2) throws IOException {
     String id = "https://orcid.org/0000-0001-6121-5409";
     PersonEntity person = new PersonEntity.PersonEntityBuilder()
-        .setId(id)
-        .setContactPoint("mailto:tim.luckett@uts.edu.au")
-        .setAffiliation("https://ror.org/03f0f6041")
-        .setFamilyName("Luckett")
-        .setGivenName("Tim")
-        .addProperty("name", "Tim Luckett")
-        .build();
+            .setId(id)
+            .setContactPoint("mailto:tim.luckett@uts.edu.au")
+            .setAffiliation("https://ror.org/03f0f6041")
+            .setFamilyName("Luckett")
+            .setGivenName("Tim")
+            .addProperty("name", "Tim Luckett")
+            .build();
     RoCrate c1 = new RoCrate.RoCrateBuilder("mini", "test", "2024", "https://creativecommons.org/licenses/by-nc-sa/3.0/au/").build();
     RoCrate c2 = new RoCrate.RoCrateBuilder("other", "with file", "2024", "https://creativecommons.org/licenses/by-nc-sa/3.0/au/")
-        .addContextualEntity(person)
-        .build();
-    writeMetadataToFile(temp1, c1);
-    writeMetadataToFile(temp2, c2);
+            .addContextualEntity(person)
+            .build();
+    this.saveCrate(c1, temp1);
+    this.saveCrate(c2, temp2);
     // some first checks...
     assertEquals(0, c1.getAllContextualEntities().size());
     assertEquals(1, c2.getAllContextualEntities().size());
@@ -81,150 +79,5 @@ class FolderReaderTest {
     assertEquals(0, c1_read.getAllContextualEntities().size());
     assertEquals(1, c2_read.getAllContextualEntities().size());
     HelpFunctions.compareTwoMetadataJsonNotEqual(c1_read, c2_read);
-  }
-
-  @Test
-  void testWithFile(@TempDir Path temp) throws IOException {
-    Path cvs = temp.resolve("survey-responses-2019.csv");
-    FileUtils.touch(cvs.toFile());
-    FileUtils.writeStringToFile(cvs.toFile(), "fkdjaflkjfla", Charset.defaultCharset());
-
-    RoCrate roCrate = new RoCrate.RoCrateBuilder("minimal", "minimal RO_crate", "2024", "https://creativecommons.org/licenses/by-nc-sa/3.0/au/")
-        .addDataEntity(
-            new FileEntity.FileEntityBuilder()
-                .setLocationWithExceptions(cvs)
-                .setId(cvs.toFile().getName())
-                .addProperty("name", "Survey responses")
-                .addProperty("contentSize", "26452")
-                .addProperty("encodingFormat", "text/csv")
-                .build()
-        )
-        .build();
-
-    assertEquals(1, roCrate.getAllDataEntities().size());
-
-    writeMetadataToFile(temp, roCrate);
-
-    CrateReader<String> reader = Readers.newFolderReader();
-    RoCrate res = reader.readCrate(temp.toFile().toString());
-    HelpFunctions.compareTwoCrateJson(roCrate, res);
-  }
-
-  @Test
-  void testWithFileUrlEncoded(@TempDir Path temp) throws IOException {
-
-    // get the std output redirected, so we can see if there is something written
-    PrintStream standardOut = System.out;
-    ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
-    System.setOut(new PrintStream(outputStreamCaptor));
-
-    Path csv = temp.resolve("survey responses 2019.csv"); // This URL will be encoded because of whitespaces
-    FileUtils.touch(csv.toFile());
-    FileUtils.writeStringToFile(csv.toFile(), "fkdjaflkjfla", Charset.defaultCharset());
-
-    RoCrate roCrate = new RoCrate.RoCrateBuilder("minimal", "minimal RO_crate", "2024", "https://creativecommons.org/licenses/by-nc-sa/3.0/au/")
-            .addDataEntity(
-                    new FileEntity.FileEntityBuilder()
-                            .setLocationWithExceptions(csv)
-                            .setId(csv.toFile().getName())
-                            .addProperty("name", "Survey responses")
-                            .addProperty("contentSize", "26452")
-                            .addProperty("encodingFormat", "text/csv")
-                            .build()
-            )
-            .build();
-
-    writeMetadataToFile(temp, roCrate);
-
-    CrateReader<String> reader = Readers.newFolderReader();
-    Crate res = reader.readCrate(temp.toFile().toString());
-    HelpFunctions.compareTwoCrateJson(roCrate, res);
-
-    // Make sure we did not print any errors
-    assertEquals("", outputStreamCaptor.toString().trim());
-    System.setOut(standardOut);
-  }
-
-  @Test
-  void TestWithFileWithLocation(@TempDir Path temp) throws IOException {
-    Path file = temp.resolve("survey-responses-2019.csv");
-    FileUtils.writeStringToFile(file.toFile(), "fakecsv.1", Charset.defaultCharset());
-    RoCrate roCrate = new RoCrate.RoCrateBuilder("minimal", "minimal RO_crate", "2024", "https://creativecommons.org/licenses/by-nc-sa/3.0/au/")
-        .addDataEntity(
-            new FileEntity.FileEntityBuilder()
-                .addProperty("name", "Survey responses")
-                .addProperty("contentSize", "26452")
-                .addProperty("encodingFormat", "text/csv")
-                .setLocationWithExceptions(file)
-                .setId("survey-responses-2019.csv")
-                .build()
-        )
-        .setPreview(null)//disable preview to allow to compare folders before and after
-        .build();
-    Path locationSource = temp.resolve("src");
-    FileUtils.forceMkdir(locationSource.toFile());
-    RoCrateWriter writer = new RoCrateWriter(new FolderWriter());
-
-    writer.save(roCrate, locationSource.toFile().toString());
-
-    writeMetadataToFile(temp, roCrate);
-
-    CrateReader<String> reader = Readers.newFolderReader();
-
-    Crate res = reader.readCrate(locationSource.toFile().toString());
-
-    Path destinationDir = temp.resolve("result");
-    FileUtils.forceMkdir(destinationDir.toFile());
-
-    writer.save(res, destinationDir.toFile().toString());
-
-    // that copies the directory locally to see its content
-    //FileUtils.copyDirectory(locationSource.toFile(), new File("test"));
-    assertTrue(HelpFunctions.compareTwoDir(locationSource.toFile(), destinationDir.toFile()));
-    HelpFunctions.compareTwoCrateJson(roCrate, res);
-  }
-
-  @Test
-  void TestWithFileWithLocationAddEntity(@TempDir Path temp) throws IOException {
-    Path file = temp.resolve("file.csv");
-    FileUtils.writeStringToFile(file.toFile(), "fakecsv.1", Charset.defaultCharset());
-    RoCrate roCrate = new RoCrate.RoCrateBuilder("minimal", "minimal RO_crate", "2024", "https://creativecommons.org/licenses/by-nc-sa/3.0/au/")
-        .addDataEntity(
-            new FileEntity.FileEntityBuilder()
-                .addProperty("name", "Survey responses")
-                .addProperty("contentSize", "26452")
-                .addProperty("encodingFormat", "text/csv")
-                .setLocationWithExceptions(file)
-                .setId("survey-responses-2019.csv")
-                .build()
-        )
-        .build();
-    Path locationSource = temp.resolve("src");
-    FileUtils.forceMkdir(locationSource.toFile());
-    RoCrateWriter writer = new RoCrateWriter(new FolderWriter());
-
-    writer.save(roCrate, locationSource.toFile().toString());
-
-    writeMetadataToFile(temp, roCrate);
-
-    CrateReader<String> reader = Readers.newFolderReader();
-
-    Path newFile = temp.resolve("new_file");
-    FileUtils.writeStringToFile(newFile.toFile(), "fkladjsl;fjasd;lfjda;lkf", Charset.defaultCharset());
-
-    Crate res = reader.readCrate(locationSource.toFile().toString());
-    res.addDataEntity(new FileEntity.FileEntityBuilder()
-        .setEncodingFormat("setnew")
-        .setLocationWithExceptions(newFile)
-        .setId("new_file")
-        .build());
-
-    Path destinationDir = temp.resolve("result");
-    FileUtils.forceMkdir(destinationDir.toFile());
-
-    writer.save(res, destinationDir.toFile().toString());
-
-    assertFalse(HelpFunctions.compareTwoDir(locationSource.toFile(), destinationDir.toFile()));
-    HelpFunctions.compareTwoMetadataJsonNotEqual(roCrate, res);
   }
 }
--- src/test/java/edu/kit/datamanager/ro_crate/reader/ZipStreamStrategyTest.java
+++ src/test/java/edu/kit/datamanager/ro_crate/reader/ZipStreamStrategyTest.java
@@ -1,209 +1,44 @@
 package edu.kit.datamanager.ro_crate.reader;
 
 import edu.kit.datamanager.ro_crate.Crate;
-import edu.kit.datamanager.ro_crate.HelpFunctions;
-import edu.kit.datamanager.ro_crate.RoCrate;
-import edu.kit.datamanager.ro_crate.entities.data.FileEntity;
-import edu.kit.datamanager.ro_crate.writer.FolderWriter;
-import edu.kit.datamanager.ro_crate.writer.RoCrateWriter;
-import edu.kit.datamanager.ro_crate.writer.ZipWriter;
+import edu.kit.datamanager.ro_crate.writer.Writers;
 
-import org.apache.commons.io.FileUtils;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.io.TempDir;
-
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.nio.charset.Charset;
+import java.io.*;
 import java.nio.file.Path;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class ZipStreamStrategyTest {
 
-    @Test
-    void testReadingBasicCrate(@TempDir Path temp) throws IOException {
-        RoCrate roCrate = new RoCrate.RoCrateBuilder("minimal", "minimal RO_crate", "2024", "https://creativecommons.org/licenses/by-nc-sa/3.0/au/")
-                .build();
-
-        Path zipPath = temp.resolve("result.zip");
-
-        RoCrateWriter roCrateZipWriter = new RoCrateWriter(new ZipWriter());
-        // save the content of the roCrate to the dest zip
-        roCrateZipWriter.save(roCrate, zipPath.toString());
-
-        File zipFile = zipPath.toFile();
-        assertTrue(zipFile.isFile());
-
-        CrateReader<InputStream> roCrateReader = Readers.newZipStreamReader();
-        Crate res = roCrateReader.readCrate(new FileInputStream(zipFile));
-        HelpFunctions.compareTwoCrateJson(roCrate, res);
+class ZipStreamStrategyTest extends CrateReaderTest<InputStream, ZipStreamStrategy> {
+    @Override
+    protected void saveCrate(Crate crate, Path target) throws IOException {
+        Writers.newZipStreamWriter().save(crate, new FileOutputStream(target.toFile()));
+        assertTrue(target.toFile().isFile());
     }
 
-    @Test
-    void testWithFile(@TempDir Path temp) throws IOException {
-        Path cvs = temp.resolve("survey-responses-2019.csv");
-        FileUtils.touch(cvs.toFile());
-        FileUtils.writeStringToFile(cvs.toFile(), "fkdjaflkjfla", Charset.defaultCharset());
-        RoCrate roCrate = new RoCrate.RoCrateBuilder("minimal", "minimal RO_crate", "2024", "https://creativecommons.org/licenses/by-nc-sa/3.0/au/")
-                .addDataEntity(
-                        new FileEntity.FileEntityBuilder()
-                                .setLocationWithExceptions(cvs)
-                                .setId(cvs.toFile().getName())
-                                .addProperty("name", "Survey responses")
-                                .addProperty("contentSize", "26452")
-                                .addProperty("encodingFormat", "text/csv")
-                                .build()
-                )
-                .build();
-
-        assertEquals(1, roCrate.getAllDataEntities().size());
-
-        Path zipPath = temp.resolve("result.zip");
-
-        RoCrateWriter roCrateZipWriter = new RoCrateWriter(new ZipWriter());
-        // save the content of the roCrate to the dest zip
-        roCrateZipWriter.save(roCrate, zipPath.toFile().getAbsolutePath());
-
-        CrateReader<InputStream> roCrateFolderReader = Readers.newZipStreamReader();
-        Crate res = roCrateFolderReader.readCrate(new FileInputStream(zipPath.toFile()));
-
-        HelpFunctions.compareTwoCrateJson(roCrate, res);
+    @Override
+    protected Crate readCrate(Path source) throws IOException {
+        return Readers.newZipStreamReader().readCrate(new FileInputStream(source.toFile()));
     }
 
-    @Test
-    void TestWithFileWithLocation(@TempDir Path temp) throws IOException {
-        Path file = temp.resolve("survey-responses-2019.csv");
-        FileUtils.writeStringToFile(file.toFile(), "fakecsv.1", Charset.defaultCharset());
-        RoCrate roCrate = new RoCrate.RoCrateBuilder("minimal", "minimal RO_crate", "2024", "https://creativecommons.org/licenses/by-nc-sa/3.0/au/")
-                .addDataEntity(
-                        new FileEntity.FileEntityBuilder()
-                                .addProperty("name", "Survey responses")
-                                .addProperty("contentSize", "26452")
-                                .addProperty("encodingFormat", "text/csv")
-                                .setLocationWithExceptions(file)
-                                .setId("survey-responses-2019.csv")
-                                .build()
-                )
-                .setPreview(null)//disable preview to allow to compare folders before and after
-                .build();
-
-        Path zipPath = temp.resolve("result.zip");
-
-        RoCrateWriter roCrateZipWriter = new RoCrateWriter(new ZipWriter());
-        // save the content of the roCrate to the dest zip
-        roCrateZipWriter.save(roCrate, zipPath.toString());
-
-        CrateReader<InputStream> roCrateFolderReader = Readers.newZipStreamReader();
-        Crate res = roCrateFolderReader.readCrate(new FileInputStream(zipPath.toFile()));
-
-        Path locationSource = temp.resolve("expected");
-        RoCrateWriter writer = new RoCrateWriter(new FolderWriter());
-        writer.save(roCrate, locationSource.toString());
-
-        Path destinationDir = temp.resolve("result");
-        FileUtils.forceMkdir(destinationDir.toFile());
-        writer.save(res, destinationDir.toString());
-
-        // that copies the directory locally to see its content
-        assertTrue(HelpFunctions.compareTwoDir(locationSource.toFile(), destinationDir.toFile()));
-        HelpFunctions.compareTwoCrateJson(roCrate, res);
-    }
-
-    @Test
-    void TestWithFileWithLocationAddEntity(@TempDir Path temp) throws IOException {
-        Path file = temp.resolve("file.csv");
-        FileUtils.writeStringToFile(file.toFile(), "fakecsv.1", Charset.defaultCharset());
-        RoCrate roCrate = new RoCrate.RoCrateBuilder("minimal", "minimal RO_crate", "2024", "https://creativecommons.org/licenses/by-nc-sa/3.0/au/")
-                .addDataEntity(
-                        new FileEntity.FileEntityBuilder()
-                                .addProperty("name", "Survey responses")
-                                .addProperty("contentSize", "26452")
-                                .addProperty("encodingFormat", "text/csv")
-                                .setLocationWithExceptions(file)
-                                .setId("survey-responses-2019.csv")
-                                .build()
-                )
-                .build();
-
-        Path zipPath = temp.resolve("result.zip");
-
-        RoCrateWriter roCrateZipWriter = new RoCrateWriter(new ZipWriter());
-        // save the content of the roCrate to the dest zip
-        roCrateZipWriter.save(roCrate, zipPath.toString());
-
-        CrateReader<InputStream> roCrateFolderReader = Readers.newZipStreamReader();
-        Crate res = roCrateFolderReader.readCrate(new FileInputStream(zipPath.toFile()));
-
-        Path locationSource = temp.resolve("expected");
-        RoCrateWriter writer = new RoCrateWriter(new FolderWriter());
-        writer.save(roCrate, locationSource.toString());
-
-        Path newFile = temp.resolve("new_file");
-        FileUtils.writeStringToFile(newFile.toFile(), "fkladjsl;fjasd;lfjda;lkf", Charset.defaultCharset());
-
-        res.addDataEntity(new FileEntity.FileEntityBuilder()
-                .setEncodingFormat("setnew")
-                .setLocationWithExceptions(newFile)
-                .setId("new_file")
-                .build());
-
-        Path destinationDir = temp.resolve("result");
-        FileUtils.forceMkdir(destinationDir.toFile());
-        writer.save(res, destinationDir.toFile().toString());
-
-        assertFalse(HelpFunctions.compareTwoDir(locationSource.toFile(), destinationDir.toFile()));
-        HelpFunctions.compareTwoMetadataJsonNotEqual(roCrate, res);
-    }
-
-    @Test
-    void testReadingBasicCrateWithCustomPath(@TempDir Path temp) throws IOException {
-        RoCrate roCrate = new RoCrate.RoCrateBuilder(
-                "minimal",
-                "minimal RO_crate",
-                "2024",
-                "https://creativecommons.org/licenses/by-nc-sa/3.0/au/")
-                .build();
-
-        Path zipPath = temp.resolve("result.zip");
-
-        RoCrateWriter roCrateZipWriter = new RoCrateWriter(new ZipWriter());
-        roCrateZipWriter.save(roCrate, zipPath.toString());
-
-        File zipFile = zipPath.toFile();
-        assertTrue(zipFile.isFile());
-
-        Path differentFolder = temp.resolve("differentFolder");
-        ZipStreamStrategy readerType = new ZipStreamStrategy(differentFolder, true);
-        assertFalse(readerType.isExtracted());
-        assertEquals(readerType.getTemporaryFolder().getFileName().toString(), readerType.getID());
-        assertTrue(readerType.getTemporaryFolder().startsWith(differentFolder));
-
-        CrateReader<InputStream> reader = new CrateReader<>(readerType);
-        Crate crate = reader.readCrate(new FileInputStream(zipFile));
-        assertTrue(readerType.isExtracted());
-        HelpFunctions.compareTwoCrateJson(roCrate, crate);
-
-        {
-            // try it again without the UUID subfolder and test if the directory is being cleaned up (using coverage).
-            ZipStreamStrategy newReaderType = new ZipStreamStrategy(differentFolder, false);
-            assertFalse(newReaderType.isExtracted());
-            assertNotEquals(newReaderType.getTemporaryFolder().getFileName().toString(), newReaderType.getID());
-            assertTrue(newReaderType.getTemporaryFolder().startsWith(differentFolder));
-
-            CrateReader<InputStream> reader2 = new CrateReader<>(newReaderType);
-            Crate crate2 = reader2.readCrate(new FileInputStream(zipFile));
-            assertTrue(newReaderType.isExtracted());
-            HelpFunctions.compareTwoCrateJson(roCrate, crate2);
+    @Override
+    protected ZipStreamStrategy newReaderStrategyWithTmp(Path tmpDirectory, boolean useUuidSubfolder) {
+        ZipStreamStrategy strategy = new ZipStreamStrategy(tmpDirectory, useUuidSubfolder);
+        assertFalse(strategy.isExtracted());
+        if (useUuidSubfolder) {
+            assertEquals(strategy.getTemporaryFolder().getFileName().toString(), strategy.getID());
+        } else {
+            assertEquals(strategy.getTemporaryFolder().getFileName().toString(), tmpDirectory.getFileName().toString());
         }
+        assertTrue(strategy.getTemporaryFolder().startsWith(tmpDirectory));
+        return strategy;
+    }
 
-        {
-            // show we can also do it with the convenience API
-            CrateReader<InputStream> reader2 = Readers.newZipStreamReader(differentFolder, false);
-            Crate crate2 = reader2.readCrate(new FileInputStream(zipFile));
-            HelpFunctions.compareTwoCrateJson(roCrate, crate2);
-        }
+    @Override
+    protected Crate readCrate(ZipStreamStrategy strategy, Path source) throws IOException {
+        Crate importedCrate = new CrateReader<>(strategy)
+                .readCrate(new FileInputStream(source.toFile()));
+        assertTrue(strategy.isExtracted());
+        return importedCrate;
     }
 }
