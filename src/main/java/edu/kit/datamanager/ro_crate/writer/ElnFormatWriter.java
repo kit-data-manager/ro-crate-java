b25d1eb0c51210d15bfe79b34d93a09a6066b43e
Andreas Pfeil <andreas.pfeil@kit.edu>
2025-05-09 18:53:43 +0200

    fix: remove IOException documentation from usingElnStyle and withRootSubdirectory methods

--- src/main/java/edu/kit/datamanager/ro_crate/writer/ElnFormatWriter.java
+++ src/main/java/edu/kit/datamanager/ro_crate/writer/ElnFormatWriter.java
@@ -1,21 +1,15 @@
 package edu.kit.datamanager.ro_crate.writer;
 
-import java.io.IOException;
-
 public interface ElnFormatWriter<SOURCE_TYPE> extends GenericWriterStrategy<SOURCE_TYPE> {
 
     /**
      * Write in ELN format style, meaning with a root subfolder in the zip file.
      * Same as {@link #withRootSubdirectory()}.
-     *
-     * @throws IOException if an error occurs
      */
     ElnFormatWriter<SOURCE_TYPE> usingElnStyle();
 
     /**
      * Alias with more generic name for {@link #usingElnStyle()}.
-     *
-     * @throws IOException if an error occurs
      */
     default ElnFormatWriter<SOURCE_TYPE> withRootSubdirectory() {
         return this.usingElnStyle();
