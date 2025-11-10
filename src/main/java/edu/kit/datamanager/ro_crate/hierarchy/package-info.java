c23df4f72f871a81b693bd949385e728c0c6e099
Andreas Pfeil <andreas.pfeil.rs@gmail.com>
2025-11-10 12:07:16 +0100

    docs: add package documentation for the hierarchy recognition feature

---
+++ src/main/java/edu/kit/datamanager/ro_crate/hierarchy/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * This package contains classes to enable automatic recognition and construction of
+ * file and folder hierarchies within RO-Crates.
+ * <p>
+ * The main functionality is provided by the {@link edu.kit.datamanager.ro_crate.hierarchy.HierarchyRecognition}
+ * class, which analyzes the paths of data entities and establishes appropriate
+ * "hasPart" and "isPartOf" relationships to reflect the underlying file system structure.
+ */
+package edu.kit.datamanager.ro_crate.hierarchy;
